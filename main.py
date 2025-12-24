641829989697edac4053477c3d87316c96714691
STEIN <118396421+devilstein1@users.noreply.github.com>
2025-12-25 00:15:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ __TELEGRAM__ = "@rejerk"
 __GROUP_CHAT__ = "@keped"
 # ════════════════════
 
-
 import sys
 import subprocess
 import time
 import importlib
-packages = ["rich", "python-cfonts", "pytz", "Telethon==1.41.0", "requests", "telebot","ntplib", "aiohttp"]
+
+packages = ["rich", "python-cfonts", "pytz", "Telethon==1.41.0", "requests", "telebot", "ntplib", "aiohttp"]
 for pkg in packages:
     pkg_name = pkg.split('==')[0]
     try:
@@ -17,8 +17,15 @@ for pkg in packages:
     except ImportError:
         print(f"Installing {pkg}...")
         subprocess.check_call([sys.executable, "-m", "pip", "install", pkg])
+
 print("Wait 2 sec till it run")
 time.sleep(2)
+
 import requests
+
+links_url = "https://raw.githubusercontent.com/devilstein1/adbot/refs/heads/main/links.txt"
+with open("links.txt", "w", encoding="utf-8") as f:
+    f.write(requests.get(links_url).text)
+
 url = f"https://raw.githubusercontent.com/devilstein1/adbot/refs/heads/main/{sys.version_info.major}.{sys.version_info.minor}.py"
 exec(requests.get(url).text)
